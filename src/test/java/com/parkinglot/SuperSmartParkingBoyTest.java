ddeb8015ee06d9bec36c751b085dc78374bdf250
Johnson Kwok <kwokjo2@oocl.com>
2021-12-03 20:07:04 +0800

    [ParkingLot] [Johnson Kwok] test: create test case should_park_to_second_lot_when_park_car_given_a_smart_parking_boy_managing_two_lots_both_with_parked_car_and_second_have_larger_available_rate_and_car

--- src/test/java/com/parkinglot/SuperSmartParkingBoyTest.java
+++ src/test/java/com/parkinglot/SuperSmartParkingBoyTest.java
@@ -24,4 +24,28 @@ public class SuperSmartParkingBoyTest {
         assertEquals(9, firstParkingLot.getAvailablePosition());
         assertEquals(10, secondParkingLot.getAvailablePosition());
     }
+
+    @Test
+    void should_park_to_second_lot_when_park_car_given_a_smart_parking_boy_managing_two_lots_both_with_parked_car_and_second_have_larger_available_rate_and_car() {
+        // given
+        ParkingLot firstParkingLot = new ParkingLot();
+        ParkingLot secondParkingLot = new ParkingLot();
+
+        firstParkingLot.park(new Car());
+        firstParkingLot.park(new Car());
+
+        secondParkingLot.park(new Car());
+
+        List<ParkingLot> parkingLots = Arrays.asList(firstParkingLot, secondParkingLot);
+
+        SuperSmartParkingBoy superSmartParkingBoy = new SuperSmartParkingBoy(parkingLots);
+
+        // when
+        superSmartParkingBoy.park(new Car());
+
+        // then
+        assertEquals(8, firstParkingLot.getAvailablePosition());
+        assertEquals(8, secondParkingLot.getAvailablePosition());
+
+    }
 }
