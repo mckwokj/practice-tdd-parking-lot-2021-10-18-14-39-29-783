733cda9ec3f462d74b59851e7b223227786d0baa
Johnson Kwok <kwokjo2@oocl.com>
2021-12-03 13:56:58 +0800

    [ParkingLot] [Johnson Kwok] test: add test case should_return_ticket_when_park_car_given_parking_lot_and_car

---
+++ src/main/java/com/parkinglot/ParkingLot.java
@@ -0,0 +1,31 @@
+package com.parkinglot;
+
+import java.util.HashMap;
+
+public class ParkingLot {
+//    private final int DEFAULT_CAPACITY = 10;
+//    private HashMap<Ticket, Car> ticketCarMap = new HashMap<>();
+//    private int capacity;
+
+//    public ParkingLot(int capacity) {
+//        this.capacity = capacity;
+//    }
+
+//    public ParkingLot() {
+//        this.capacity = DEFAULT_CAPACITY;
+//    }
+
+    public Ticket park(Car car) {
+//        if (hasAvailablePosition()) {
+//            Ticket ticket = new Ticket();
+//            ticketCarMap.put(ticket, car);
+//            return ticket;
+//        }
+
+        return null;
+    }
+
+//    private boolean hasAvailablePosition() {
+//        return ticketCarMap.size() < capacity;
+//    }
+}
---
+++ src/test/java/com/parkinglot/ParkingLotTest.java
@@ -0,0 +1,45 @@
+package com.parkinglot;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public class ParkingLotTest {
+    @Test
+    void should_return_ticket_when_park_car_given_parking_lot_and_car() {
+        // given
+        ParkingLot parkingLot = new ParkingLot();
+        Car car = new Car();
+
+        // when
+        Ticket ticket = parkingLot.park(car);
+
+        // then
+        assertNotNull(ticket);
+    }
+
+//    @Test
+//    void should_return_null_when_park_car_given_full_parking_lot_and_car() {
+//        // given
+//        ParkingLot parkingLot = new ParkingLot(1);
+//        Car car = new Car();
+//        parkingLot.park(car);
+//
+//        // when
+//        Ticket ticket = parkingLot.park(car);
+//
+//        // then
+//        assertNull(ticket);
+//    }
+    
+//    @Test
+//    void should_return_parked_car_when_get_car_given_parking_lot_with_the_parked_car() {
+//        // given
+//
+//        // when
+//
+//        // then
+//    }
+    
+
+}
